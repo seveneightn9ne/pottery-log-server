05231e03266d0e410334e6d9320282fb2ae99f47
Miles Steele <miles@milessteele.com>
2026-03-25 08:33:13 -0400

    deploy update

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 set -e
 
 GOOS=linux GOARCH=amd64 go build
-ssh stipple "mv pottery-log-server/pottery-log-server pottery-log-server/pottery-log-server.bak"
-scp pottery-log-server stipple:pottery-log-server/pottery-log-server
-ssh stipple "sudo systemctl restart pottery-log-server"
-ssh stipple "tail pottery-log-server/pottery-log-server.log"
+ssh jessk@stipple "mv pottery-log-server/pottery-log-server pottery-log-server/pottery-log-server.bak"
+scp pottery-log-server jessk@stipple:pottery-log-server/pottery-log-server
+ssh jessk@stipple "sudo systemctl restart pottery-log-server"
+ssh jessk@stipple "tail pottery-log-server/pottery-log-server.log"
